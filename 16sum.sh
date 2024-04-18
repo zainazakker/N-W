c6c9cbf16e7808a74173ec48967db46a63350d8c
Zaina <159014285+zainazakker@users.noreply.github.com>
2024-04-18 20:35:03 +1200

    Update 16sum.sh

--- 16sum.sh
+++ 16sum.sh
@@ -15,8 +15,8 @@ $sum"
 
 <<comment
 
-mlm@mlm-desktop:~/Desktop/Rojin/NW LAB$ chmod +x 16sum.sh
-mlm@mlm-desktop:~/Desktop/Rojin/NW LAB$ ./16sum.sh
+mlm@mlm-desktop:~/Desktop/Zaina/NW LAB$ chmod +x 16sum.sh
+mlm@mlm-desktop:~/Desktop/zaina/NW LAB$ ./16sum.sh
 Sum of numbers between 50 and 100, which are divisible by 3 and not divisible by 5: 1050
 
 comment
